ed5f43c176dee9266149427c5461ef4bc83bbd0c
Kim Kao <yikaikao@gmail.com>
2017-11-27 17:45:28 +0800

    add send mail sample

--- src/main/java/solid/humank/statehandler/ResultNotifyHandler.java
+++ src/main/java/solid/humank/statehandler/ResultNotifyHandler.java
@@ -1,4 +1,19 @@
 package solid.humank.statehandler;
 
-public class ResultNotifyHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import solid.humank.model.EC2RequestResult;
+import solid.humank.model.NotifyInfo;
+
+public class ResultNotifyHandler implements RequestHandler<EC2RequestResult,NotifyInfo> {
+    @Override
+    public NotifyInfo handleRequest(EC2RequestResult input, Context context) {
+        return null;
+    }
+
+
+    public void sendEmail(){
+
+    }
+
 }
