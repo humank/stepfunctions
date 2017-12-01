a13c225c775e3690786f0fa6d660ac9d5b139cd2
Kim Kao <yikaikao@gmail.com>
2017-12-02 00:46:30 +0800

    refactor - move asgtag function to asgutil.

--- src/test/java/solid/humank/solid/humank/requestEC2/OnDemandASGEC2Test.java
+++ src/test/java/solid/humank/solid/humank/requestEC2/OnDemandASGEC2Test.java
@@ -1,15 +1,11 @@
 package solid.humank.solid.humank.requestEC2;
 
 import com.amazonaws.ResponseMetadata;
-import com.amazonaws.http.SdkHttpMetadata;
 import com.amazonaws.services.autoscaling.model.CreateAutoScalingGroupResult;
 import com.amazonaws.services.autoscaling.model.Tag;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import org.apache.logging.log4j.Logger;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.junit.Before;
 import org.junit.Test;
 import solid.humank.uitls.ASGCreator;
@@ -42,20 +38,10 @@ public class OnDemandASGEC2Test {
         imageId = "ami-da9e2cbc";
         instanceType = "t2.micro";
         targetGroupArn = "arn:aws:elasticloadbalancing:ap-northeast-1:584518143473:targetgroup/TG-lab-ALB-16NABNOLSNMWC/9f8c337c46e80d77";
-        tags = createTags();
+        tags = AsgUtil.defineInstanceTags();
         vpcIdSubnets = "subnet-77f8703e, subnet-43a36218";
         spotPrice = 0.0;
         securityGroups = "lab-SG-PKDT24OQIGEE-EC2HostSecurityGroup-GQ9GPFW3WNZF";
-    }
-
-    private List<Tag> createTags() {
-        List<Tag> tags = new ArrayList<Tag>();
-        tags.add(
-                new Tag().withKey("Name")
-                        .withValue("InstanceFromASG")
-                        .withPropagateAtLaunch(true)
-        );
-        return tags;
     }
 
     @Before
@@ -72,7 +58,7 @@ public class OnDemandASGEC2Test {
         CreateAutoScalingGroupResult result = asgCreator.requestASGEC2(asgName, launchConfigurationName, imageId, instanceType, targetGroupArn, tags, vpcIdSubnets, keyName, spotPrice, securityGroups);
 
         ResponseMetadata metadata = result.getSdkResponseMetadata();
-        assertEquals("{}",metadata.toString());
+        assertEquals("{}", result.toString());
 
 
     }
